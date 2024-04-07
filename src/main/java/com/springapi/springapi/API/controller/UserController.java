a52c8dac68ef5c694f420f3459c502116de8496a
Vignesh <vignesh.bsk@gmail.com>
2024-04-07 20:26:22 +0530

    Created basic structure

---
+++ src/main/java/com/springapi/springapi/API/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.springapi.springapi.API.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springapi.springapi.API.model.User;
+import com.springapi.springapi.Service.UsesrService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class UserController {
+    private UsesrService userService;
+
+    @Autowired
+    public UserController(UsesrService userService)
+    {
+        this.userService=userService;
+    }
+
+    @GetMapping()
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    public User getUser(@RequestParam Integer id)
+    {
+        return userService.getUser();
+        
+    }
+
+}
---
+++ src/main/java/com/springapi/springapi/Service/UsesrService.java
@@ -0,0 +1,14 @@
+package com.springapi.springapi.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.springapi.springapi.API.model.User;
+
+@Service
+public class UsesrService {
+
+    public User getUser() {
+        return null;
+    }
+    
+}
