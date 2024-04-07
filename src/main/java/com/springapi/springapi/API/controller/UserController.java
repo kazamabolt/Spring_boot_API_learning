b2fb226098d9cb501ee05c232e0d9c178249e525
Vignesh <vignesh.bsk@gmail.com>
2024-04-07 20:52:37 +0530

    Created User End point

--- src/main/java/com/springapi/springapi/API/controller/UserController.java
+++ src/main/java/com/springapi/springapi/API/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.springapi.springapi.API.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.springapi.springapi.API.model.User;
 import com.springapi.springapi.Service.UsesrService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,14 +17,10 @@ public class UserController {
         this.userService=userService;
     }
 
-    @GetMapping()
-    public String getMethodName(@RequestParam String param) {
-        return new String();
-    }
-    
+    @GetMapping("/user")
     public User getUser(@RequestParam Integer id)
     {
-        return userService.getUser();
+        return userService.getUser(id);
         
     }
 
--- src/main/java/com/springapi/springapi/Service/UsesrService.java
+++ src/main/java/com/springapi/springapi/Service/UsesrService.java
@@ -1,14 +1,36 @@
 package com.springapi.springapi.Service;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.stereotype.Service;
-
 import com.springapi.springapi.API.model.User;
 
 @Service
 public class UsesrService {
 
-    public User getUser() {
+    private List<User> userList;
+
+    public UsesrService()
+    {
+        userList=new ArrayList<>();
+
+        User user1=new User(1, "Vignesh", 23, "vicky@gmail.com");
+        User user2=new User(2, "Robin", 23, "robin@gmail.com");
+        User user3=new User(3, "Sathish", 22, "sathish@gmail.com");
+        User user4=new User(4, "Ramji", 25, "ramji@gmail.com");
+        User user5=new User(5, "Ram", 27, "gokulram@gmail.com");
+
+
+        userList.addAll(Arrays.asList(user1,user2,user3,user4,user5));
+    }
+
+    public User getUser(Integer id) {
+        for (User user : userList) {
+            if(id==user.getId())
+            {
+                return user;
+            }
+        }
         return null;
     }
-    
 }
